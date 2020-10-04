8636b41ea85bf6c87ea859cab3a4775a1c5f6f27
Jordan Lee <jbjordanleemac@gmail.com>
2020-10-04 14:55:00 -0700

    add array_1004020_01 100420 254pm

---
+++ array_100420_01.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# print the last 4 lines for a file called list without using tail 100420
+lists=()
+
+listlast4lines() {
+  for a in `cat /opt/bash_test2/list`
+  do
+    echo "$a"
+    lists=(${lists[@]} $a)
+  done
+  
+  echo "${lists[@]}"
+  
+  b=${#lists[@]}
+  echo "$b"
+  
+  for c in {1..4}
+  do
+    echo "${lists[$(( b - 1 ))]}"
+    b=$(( b - 1 ))
+  done
+}
+
+# call function
+listlast4lines
